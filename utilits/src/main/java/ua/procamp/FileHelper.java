5359b29aa222b2205e3f163be9e7a95d62d1b649
Oleksandr Roienko <OleksandrRoienko@epam.com>
2019-11-05 10:07:00 +0200

    new version

---
+++ utilits/src/main/java/ua/procamp/FileHelper.java
@@ -0,0 +1,26 @@
+package ua.procamp;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileHelper {
+
+  public static String fileReader (File file ){
+    StringBuilder s = new StringBuilder();
+    try {
+
+      Scanner scanner = new Scanner(file);
+      while (scanner.hasNextLine()) {
+
+        s.append(scanner.nextLine()+"\n");
+      }
+      scanner.close();
+    } catch (FileNotFoundException e) {
+      return "";
+    }
+    return s.length()>0?s.toString().substring(0,s.toString().length()-1):"";
+
+  }
+
+}
